bfbf3a39158ed4758072024f44ea0c9c7cd87ded
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2022-05-16 15:23:43 +0900

    N, S , E Button -> Each Activity move

--- app/src/main/java/kr/ac/kpu/ce2019152012/seeheung/MainActivity.kt
+++ app/src/main/java/kr/ac/kpu/ce2019152012/seeheung/MainActivity.kt
@@ -1,11 +1,28 @@
 package kr.ac.kpu.ce2019152012.seeheung
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.kpu.ce2019152012.seeheung.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.NBtn.setOnClickListener {
+            val intent = Intent(this,NorthMap::class.java)
+            startActivity(intent)
+        }
+        binding.SBtn.setOnClickListener {
+            val intent = Intent(this,SouthMap::class.java)
+            startActivity(intent)
+        }
+        binding.EBtn.setOnClickListener {
+            val intent = Intent(this,EastMap::class.java)
+            startActivity(intent)
+        }
     }
 }
