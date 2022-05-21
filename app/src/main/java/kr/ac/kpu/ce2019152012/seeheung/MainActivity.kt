4cb613b9dfebd5562a73710dd664b45aeb8facbf
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2022-05-22 02:01:46 +0900

    bottom navigation add

--- app/src/main/java/kr/ac/kpu/ce2019152012/seeheung/MainActivity.kt
+++ app/src/main/java/kr/ac/kpu/ce2019152012/seeheung/MainActivity.kt
@@ -1,28 +1,23 @@
 package kr.ac.kpu.ce2019152012.seeheung
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import kr.ac.kpu.ce2019152012.seeheung.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.NBtn.setOnClickListener {
-            val intent = Intent(this,NorthMap::class.java)
-            startActivity(intent)
-        }
-        binding.SBtn.setOnClickListener {
-            val intent = Intent(this,SouthMap::class.java)
-            startActivity(intent)
-        }
-        binding.EBtn.setOnClickListener {
-            val intent = Intent(this,EastMap::class.java)
-            startActivity(intent)
-        }
+        val navHostUserFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        val navController = navHostUserFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/kr/ac/kpu/ce2019152012/seeheung/MainFragments/SettingFragment.kt
@@ -0,0 +1,53 @@
+package kr.ac.kpu.ce2019152012.seeheung.MainFragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestoreSettings
+import kr.ac.kpu.ce2019152012.seeheung.databinding.FragmentSettingBinding
+
+class SettingFragment : Fragment() {
+    private var _binding: FragmentSettingBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var db: FirebaseFirestore
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setup2()
+    }
+
+    // 화면 구성
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSettingBinding.inflate(inflater,container,false)
+        val view = binding.root
+
+        return view
+    }
+
+    // 기능 넣는 곳
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    fun setup2() {
+        db = FirebaseFirestore.getInstance()
+        val settings = firestoreSettings {
+            isPersistenceEnabled = true
+        }
+        db.firestoreSettings = settings
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
